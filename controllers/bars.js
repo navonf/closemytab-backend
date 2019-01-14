26f34583dbb4d482d6bf4b17eb1613dc426ffd74
Navon Francis <navonfrancis@gmail.com>
2019-01-13 21:54:46 -0500

    up the limit.

--- controllers/bars.js
+++ controllers/bars.js
@@ -16,7 +16,7 @@ module.exports = {
   getNearest(req, res) {
     const query = req.query;
 
-    fetch(`https://api.yelp.com/v3/businesses/search?latitude=${query.lat}&longitude=${query.lng}&term=bars`, options)
+    fetch(`https://api.yelp.com/v3/businesses/search?latitude=${query.lat}&longitude=${query.lng}&term=bars&limit=50`, options, params)
       .then((data) => data.json())
       .then((data) => {
 
